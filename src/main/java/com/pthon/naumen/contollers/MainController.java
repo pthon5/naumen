7b25f8411f45de98961c2d11a071047187f43542
pthon5 <8967858b87@gmail.com>
2021-07-14 20:12:06 +0500

    first

---
+++ src/main/java/com/pthon/naumen/contollers/MainController.java
@@ -0,0 +1,16 @@
+package com.pthon.naumen.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    //Home page
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+}
---
+++ src/test/java/com/pthon/naumen/NaumenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pthon.naumen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaumenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
